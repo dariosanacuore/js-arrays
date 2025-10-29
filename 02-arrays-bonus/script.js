bb6ee91d74ff065432ca8d4f204b2ef0135076a4
dariosanacuore <dariosanacuore2@gmail.com>
2025-10-29 16:25:32 +0100

    feat:Completati i primi 2 esercizi del bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,28 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+let reversedTeachers = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers = teachers[i];
+  console.log(reversedTeachers);
+}
+
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const CurrentName = teachers[i];
+  if (CurrentName.length >= 5) {
+    longNames.push(CurrentName);
+  }
+
+}
+
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
